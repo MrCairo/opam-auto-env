2dd2babfa53be361d0fa8ec1b953db840299c647
Mitch Fisher <mitch@committed-code.com>
2024-07-15 18:14:54 -0400

    Updates to README.md with regards to using the :ensure-system-package option.

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # opam-auto-env
+
+## Abstract
+opam-auto-env will set the OPAM environment variables as if `eval $(opam env)` is called - which, in fact, it is called
+if an opam directory named `_opam` or `.opam` is found withing the current directory or within it's parent directories.
+If one is found, then the `opam env` command is called via the Emacs `shell-command-to-string` function. If none are
+found, then the existing `opam` environment (if one existed) is left unchanged.
+
+The resulting environment variables returned from `opam env` are then set into the current Emacs session, including the
+PATH, which is copied over to the `exec-path` list. This is important to note since anything custom in the `exec-path`
+list that wasn't already in the current shell's path, will be lost.
+
+## Installation
+You can install the package from Melpa:
+
+M-x package-install RET opam-auto-env
+
+or by using `(use-package opam-auto-mode :ensure t)`.
+
+## Usage
+You can enable opam-auto-env below.
+
+If installing via `elpaca`:
+```
+(use-package opam-auto-mode
+  :ensure (:host github :repo "MrCairo/opam-auto-env"))
+```
+
+Standard installation:
+
+```
+(use-package opam-auto-mode
+  :ensure t
+  :hook
+  ((tuareg-mode . opam-auto-env-set)
+    (dune-mode . opam-auto-env-set)))
+```
+
+To get fancy, you could also download and install `opam` if it isn't already installed using the
+`:ensure-system-package` option.
+
+```
+(let
+  ((installer "bash -c \"sh <(curl -fsSL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh) --version 2.2.0\""))
+  (use-package opam-auto-mode
+    :ensure t
+    :ensure-system-package (opam . installer)))
+```
+
+Using any of the configurations above, when a file is opened in the `dune-mode` or `tuareg-mode` to become active, the
+`opam` environment in Emacs will also be automatically updated. It is also possible to manually run the interactive
+`opam-auto-env-set` function and it will search and possibly set the `opam` environment based upon the current buffer's
+directory (not necessarily the file loaded).
+
+The default `opam` signature directory names searched for are located in the customizable variable list 
+`opam-auto-env-sig-dirnames`. The directories are searched for in the order they appear in this list.
+
+The current operating `opam` environment directory can be found by either inspecting the `OPAM_SWITCH_PREFIX`
+environment variable or by inspecting the package `opam-auto-env-current` variable.
---
+++ opam-auto-env.el
@@ -0,0 +1,115 @@
+;;; opam-auto-env.el --- Automatically find and use opam environment -*- lexical-binding: t -*-
+
+;; Copyright (C) 2024 Mitch Fisher <MrCairo@committed-code.com>
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "28+") (opam "2.0.1"))
+;; URL: https://github.com/MrCairo/opam-auto-env
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
+
+;;; Commentary:
+
+;; opam-auto-env, by calling the function interactive function
+;; `opam-auto-env-set`, will automatically setup the OPAM environment
+;; variables. This is done only if a `_opam` or `.opam` sub-directory exists in
+;; the current working directory. If one of the opam signature directories
+;; exist, opam-auto-env will execute a shell command, "eval $(opam env)" and
+;; then set the Emacs environment variables for OPAM accordingly. If neither of
+;; the signature directorie exist, then the current OPAM environment (if there
+;; is one) is left unchanged. The signature directories are searched in the
+;; order they appear in the customizable list, `opam-auto-env-dirnames`.
+
+;;; Code:
+
+(defgroup opam-auto-env-mode nil
+  "Autmatic switcher of opam environment."
+  :prefix "opam-auto-env-"
+  :group 'opam-auto-env)
+
+(defcustom opam-auto-env-sig-dirnames
+  (list "_opam" ".opam")
+  "The signature directory names that OPAM can use to identify
+it's environment."
+  :type '(repeat string))
+
+(defvar opam-auto-env-current nil
+  "The current OPAM environment directory.")
+
+(defun opam-auto-env--locate-env
+  (base-directory dirname)
+  "Search for a sub-directory name in the base directory.
+If the sub-directory name is found, the full path to the base
+Search for a venv that matches VENV-DIRNAME
+from BASE-DIRECTORY.  The behavior is similar to
+`locate-dominating-file'."
+  (when-let ((parent-dir (locate-dominating-file base-directory dirname)))
+    (expand-file-name
+      (concat (file-name-as-directory parent-dir) dirname))))
+
+(defun opam-auto-env--locate-envs
+  (base-directory sig-dirnames)
+  "Search for a venv directory from venv directories.
+Search for VENV-DIRNAMES from BASE-DIRECTORY.
+The behavior is similar to `locate-dominating-file'.
+The priority is same as the order of VENV-DIRNMAES.
+Return a path of the venv directory or nil."
+  (seq-some #'identity
+    (mapcar
+      (lambda (dirname)
+	(opam-auto-env--locate-env base-directory dirname))
+      sig-dirnames)))
+
+(defun opam-auto-env-set ()
+  "Search for a venv directory and activate it."
+  (interactive)
+  (when-let* ((opam-env-dir (opam-auto-env--locate-envs
+			      default-directory
+			      opam-auto-env-sig-dirnames))
+	       (match (not (equal opam-env-dir opam-auto-env-current))))
+    (opam-auto-env--set-env opam-env-dir)))
+
+(defun opam-auto-env--set-env (env-dir)
+  "Set the Emacs environment variables based upon the returned $(opam env)."
+  (setq opam-auto-env-curr-dir default-directory)
+  (cd env-dir)
+  (setq-default opam-auto-env-current env-dir)
+  (let*
+    ((all
+       (shell-command-to-string
+	 (format "eval $(opam env); echo -n \"%s|%s|%s|%s|%s\""
+	   "${OPAM_SWITCH_PREFIX}"
+	   "${CAML_LD_LIBRARY_PATH}"
+	   "${OCAML_TOPLEVEL_PATH}"
+	   "${OPAM_LAST_ENV}"
+	   "${PATH}")))
+      (env-list (split-string all "|"))
+      (prefix (nth 0 env-list))
+      (lib-path (nth 1 env-list))
+      (toplevel (nth 2 env-list))
+      (last-env (nth 3 env-list))
+      (path (nth 4 env-list)))
+    (setenv "OPAM_SWITCH_PREFIX" prefix)
+    (setenv "CAML_LD_LIBRARY_PATH" lib-path)
+    (setenv "OCAML_TOPLEVEL_PATH" toplevel)
+    (setenv "PATH" path)
+    (setenv "OPAM_LAST_ENV" last-env))
+  (when-let* ( (dir-list (split-string (getenv "PATH") ":")))
+    (setq-default exec-path dir-list))
+  (cd opam-auto-env-curr-dir))
+
+(provide 'opam-auto-env)
+;;; opam-auto-env.el ends here
